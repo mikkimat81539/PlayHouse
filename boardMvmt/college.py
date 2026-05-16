cacfefb94b6e349bcd82e8b03c592c9ed60b2aa0
mikkimat81539 <mmatthews81539@gmail.com>
2026-05-16 21:06:06 +0000

    created function for payment option

--- boardMvmt/college.py
+++ boardMvmt/college.py
@@ -1,17 +1,36 @@
 import random
 
+class STATS:
+	def __init__(self, name, age, major, debt):
+		self.name = name
+		self.age = 18
+		self.major = "undecided"
+		self.debt = 0	
+
 def scholarship():
 	dice = random.randint(1, 20)
-	print(dice)	
 	if dice <= 10:
 		print("You did not receive the scholarship reward.")
-	elif 10 < dice <= 15:
+	elif 10 < dice <= 17:
 		print("You received partial scholarship")
 
 	else:
 		print("You got a full ride scholarship")	
 		
 
+def loan():
+	pass
+
+def parents():
+	dice = random.randint(1, 20)
+	if dice <= 10:
+		print("Your parents choice not to support you.")
+	elif 10 < dice <= 17:
+		print("Your parents agreed to pay half")
+
+	else:
+		print("Your parents covered the full cost of your tuition")	
+ 
 
 def main():
 	print("You graduate high school, and start college.\n")
@@ -25,10 +44,10 @@ def main():
 		scholarship()
 
 	elif paymentOption == "LOAN":
-		pass
+		loan()
 
 	elif paymentOption == "PARENTS":
-		pass
+		parents()
 
 	else:
 		print("Invalid Input")
